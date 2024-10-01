812e70ca7babed5ae33c650ebb4a53c350cc2482
RomildoPessoa <rfpessoa3001@gmail.com>
2024-10-01 14:49:19 -0300

    ATUALIZADO

--- 1° Projeto/script.js
+++ 1° Projeto/script.js
@@ -1,2 +1,16 @@
-var msg = window.document.getElementById('img')
-var img = window.document.getElementById('foto')
+function carregar() {
+    var msg = window.document.getElementById('img')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    img.innerHTML = `Agora são ${hora} horas.`
+    if ( hora >= 0 && hora < 12){
+        // BOM DIA
+    }
+    else if (hora => 12 && hora < 18){
+        //BOA TARDE
+    }
+    else{
+        //BOA NOITE
+}
+}
