3b8c7f3cd086601b9cc1bb7f12636ee3c99ab3ae
RomildoPessoa <rfpessoa3001@gmail.com>
2024-10-06 11:53:28 -0300

    atualiza

--- 2° trabalho/script.js
+++ 2° trabalho/script.js
@@ -1,6 +1,15 @@
-function checar(){
-    window.alert ('oi')
-    /*var data = new Date()
-    var ano = getfullYear()
-    var fano = getE*/
-}
+function verificar(){
+    //window.alert('fui')}
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById('txtano')
+    var res = window.document.getElementById('res')
+    
+    if (fano.value. length == 0 || fano.value > ano){
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    }else{
+        var fano = document.getElementsByName('radsex')
+        var idade = ano - Number('fano.value')
+        res.innerHTML =`Sua idade é: ${idade}`
+    }
+    }
