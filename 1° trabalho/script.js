fc07f87aeec5270d07b250b923d6e816a927d92f
RomildoPessoa <rfpessoa3001@gmail.com>
2024-10-06 15:53:12 -0300

    atual

--- 1° trabalho/script.js
+++ 1° trabalho/script.js
@@ -2,21 +2,19 @@
         var menssagem = window.document.getElementById('noticia')
         var img = window.document.getElementById('imagem')
         var data = new Date()
-        var hora = data.getHours()
-        //var hora = 13
+        //var hora = data.getHours()
+        var hora = 20
         menssagem.innerHTML = `Agora são ${hora} horas`
         if ( hora >= 0 && hora < 12 ){
             // BOM DIA
             img.scr = 'fotomanha.png'
-            document.body.style.background= '#e2cd9f'
+           
         }
         else if(hora >= 12 && hora < 18 ){
             //BOA TARDE
             img.src = 'fototarde.png'
-            document.body.style.background = '#b9846f'
-        }
+        }  
         else {
             img.src = 'fotonoite.png'
-            document.body.style.background = '#515154'
-        }
+        }   
     }
